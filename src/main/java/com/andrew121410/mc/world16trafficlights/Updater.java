41841499cddcacc704558d0ffbea345185836da9
Andrew121410 <andrewcherring@gmail.com>
2023-01-06 17:10:54 -0500

    2023!!! (bumped dependencies)

--- src/main/java/com/andrew121410/mc/world16trafficlights/Updater.java
+++ src/main/java/com/andrew121410/mc/world16trafficlights/Updater.java
@@ -1,8 +1,8 @@
 package com.andrew121410.mc.world16trafficlights;
 
-import com.andrew121410.mc.world16utils.utils.ccutils.utils.AbstractBasicSelfUpdater;
+import com.andrew121410.mc.world16utils.utils.ccutils.utils.HashBasedUpdater;
 
-public class Updater extends AbstractBasicSelfUpdater {
+public class Updater extends HashBasedUpdater {
 
     private static final String JAR_URL = "https://github.com/World1-6/World1-6TrafficLights/releases/download/latest/World1-6TrafficLights.jar";
     private static final String HASH_URL = "https://github.com/World1-6/World1-6TrafficLights/releases/download/latest/hash.txt";
